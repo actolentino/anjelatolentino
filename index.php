5517bbbeeff1aed83710d26d2c6e31b529ebe090
actolentino <102888104+actolentino@users.noreply.github.com>
2022-04-06 11:43:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+
+
 <form action="site.php" method="GET">
      Num1: <input type="number" name="num1">
      Num2: <input type="number" name="num2">
      <input type="submit">
+     
 </form>
 
 <?php
+ echo "<body style='background-color:pink'>";
      $num1 = $_GET["num1"];
      $num2 = $_GET["num2"];
      echo $num1 + $num2;
