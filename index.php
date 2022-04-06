d0dc28f386a9ad761b3969b7d9ce4fa67882b75b
actolentino <102888104+actolentino@users.noreply.github.com>
2022-04-06 11:50:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 Welcome to My Simple Calculator
 
 <form action="site.php" method="GET">
-     Num1: <input type="number" name="num1">
-     Num2: <input type="number" name="num2">
+     Input Num1: <input type="number" name="num1">
+     Input Num2: <input type="number" name="num2">
      <input type="submit">
      
 </form>
 
 <?php
  echo "<body style='background-color:red'>";
-     $num1 = $_GET["input number1"];
-     $num2 = $_GET["input number2"];
+     $num1 = $_GET["num1"];
+     $num2 = $_GET["num2"];
      echo $num1 + $num2;
 ?>
